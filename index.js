8147396552fdd2c4a20157bdcded0517d2f085bd
Diego Pagini <diego.pagini@neoris.com>
2022-08-31 10:08:33 +0200

    city2

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 /** @format */
-import { inquirerMenu, pause } from './helpers/inquirer.js';
+import 'colors';
+
+import { inquirerMenu, pause, readInput } from './helpers/inquirer.js';
+import { Searches } from './models/searches.js';
 
 const main = async () => {
+	const search = new Searches();
 	let option;
 
 	do {
 		option = await inquirerMenu();
-		await pause();
+
+		switch (option) {
+			case 1:
+				const city = await readInput('\nCiudad: ');
+
+				console.log('\nInformación de la ciudad\n'.green);
+				console.log('Ciudad:');
+				console.log('Lat:');
+				console.log('Lng:');
+				console.log('Temperatura:');
+				console.log('Mínima:');
+				console.log('Máxima:');
+
+				break;
+
+			case 2:
+				break;
+		}
+
+		if (option !== 0) await pause(); // The pause is going to be shown only if the selected option is not "go out".
 	} while (option !== 0);
 };
 
---
+++ models/searches.js
@@ -0,0 +1,14 @@
+/** @format */
+
+export class Searches {
+	constructor() {
+		// In JS we can create properties in the constructor without been previously declared.
+		this.history = [];
+	}
+
+	async searchCity(city = '') {
+		console.log(city);
+
+		return [];
+	}
+}
